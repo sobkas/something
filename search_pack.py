7890fb1b7a0d27b332256abfca5628d8f156a641
Krzysztof Sobiecki <sobkas@sobkas.io>
2022-08-27 01:00:26 +0200

    Add search_pack.py

--- search_pack.py
+++ search_pack.py
@@ -6,16 +6,6 @@ import tempfile
 import shutil
 import json
 
-def neighborhood(iterable):
-    iterator = iter(iterable)
-    prev_item = None
-    current_item = next(iterator)  # throws StopIteration if empty.
-    for next_item in iterator:
-        yield (prev_item, current_item, next_item)
-        prev_item = current_item
-        current_item = next_item
-    yield (prev_item, current_item, None)
-
 tmpdir = tempfile.mkdtemp()
 print(tmpdir)
 apt_pkg.init()
